fe8fd0eac43391aa4c58d883af0331e31c88b54a
HPdeksjet2130 <scherbininsemen983@gmail.com>
2025-03-17 05:59:43 +0000

    Modifications

--- circle.h
+++ circle.h
@@ -11,9 +11,16 @@ using namespace std;
 void circle(float rad, float alpha)
 {
     float pi = 3.1415926535;
-    cout << "Длина окружности: " << 2.0 * pi * rad << endl;
-    cout << "Площадь окружности: " << pi * rad * rad << endl;
-    cout << "Площадь сектора окружности: " << pi * rad * rad * (alpha / 360.0);
+    if (rad <= 0)
+        cout << "Радиус не может быть меньше нуля" << endl;
+    else if (alpha < 0 || alpha > 360)
+        cout << "Угол не может быть меньше 0 и больше 360" << endl;
+    else 
+    {
+        cout << "Длина окружности: " << 2.0 * pi * rad << endl;
+        cout << "Площадь окружности: " << pi * rad * rad << endl;
+        cout << "Площадь сектора окружности: " << pi * rad * rad * (alpha / 360.0) << endl;
+    }
 }
 
 
--- rectangle.h
+++ rectangle.h
@@ -9,9 +9,14 @@ using namespace std;
 
 void rectangle(float side1, float side2)
 {
-    cout << "Периметр: " << (side1 + side2) * 2.0 << endl;
-    cout << "Площадь: " << side1 * side2 << endl;
-    cout << "Диагональ: " << sqrt(side1 * side1 + side2 * side2) << endl;
+    if (side1 <= 0 || side2 <= 0)
+        cout << "Ошибка" << endl;
+    else 
+    {
+        cout << "Периметр: " << (side1 + side2) * 2.0 << endl;
+        cout << "Площадь: " << side1 * side2 << endl;
+        cout << "Диагональ: " << sqrt(side1 * side1 + side2 * side2) << endl;
+    }
 }
 
 
