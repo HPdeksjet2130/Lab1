3bec0b52f8d44ac62b42d21ccd683184879e8e30
HPdeksjet2130 <scherbininsemen983@gmail.com>
2025-03-17 06:20:37 +0000

    Modified

--- triangle.h
+++ triangle.h
@@ -11,17 +11,24 @@ using namespace std;
 void triangle(float side1, float side2, float side3)
 {
     float semiper;
-    semiper = (side1 + side2 + side3) / 2.0;
-    cout << "Периметр: " << semiper * 2.0 << endl;
-    cout << "Площадь по формуле Герона: " << sqrt(semiper * (semiper - side1) * (semiper - side2) * (semiper - side3)) << endl;
-    if (side1 == side2 || side1 == side3 || side2 == side3)
+    if (side1 <= 0 || side2 <= 0 || side3 <= 0)
+        cout << "Стороны не могут быть меньше нуля или равняться ему" << endl;
+    else if ((side1 + side2 <= side3) || (side1 + side3 <= side2) || (side3 + side2 <= side1))
+        cout << "Такого треугольника не существует" << endl;
+    else 
     {
-        cout << "Равнобедренный";
-    }
-    else
-    {
-        cout << "Неравнобедренный";
-    }
+        semiper = (side1 + side2 + side3) / 2.0;
+        cout << "Периметр: " << semiper * 2.0 << endl;
+        cout << "Площадь по формуле Герона: " << sqrt(semiper * (semiper - side1) * (semiper - side2) * (semiper - side3)) << endl;
+        if (side1 == side2 || side1 == side3 || side2 == side3)
+        {
+            cout << "Равнобедренный" << endl;
+        }
+        else
+        {
+            cout << "Неравнобедренный" << endl;
+        }
+    }    
 }
 
 
